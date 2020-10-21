b76b0de2526a3f4f6061d484e316ddedbe0a4759
okira <omarrafat60@gmail.com>
2020-10-21 19:17:12 +0300

    Created the header for the admin panel page

---
+++ frontend-angular/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -0,0 +1,17 @@
+import { UsersService } from './../services/users.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-dashboard',
+  templateUrl: './admin-dashboard.component.html',
+  styleUrls: ['./admin-dashboard.component.css'],
+})
+export class AdminDashboardComponent implements OnInit {
+  constructor(private usersService: UsersService) {}
+
+  ngOnInit(): void {}
+
+  public logout(): void {
+    this.usersService.logout();
+  }
+}
