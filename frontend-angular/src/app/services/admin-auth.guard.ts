a71c34bd60086cc8a8ae660924ee5b7933d28f2e
okira <omarrafat60@gmail.com>
2020-10-21 19:18:43 +0300

    Created an auth guard for the admin dashboard page

---
+++ frontend-angular/src/app/services/admin-auth.guard.ts
@@ -0,0 +1,49 @@
+import { UsersService } from './users.service';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AdminAuthGuard implements CanActivate {
+  private isAdmin: boolean = false;
+
+  constructor(
+    private usersService: UsersService,
+    private http: HttpClient,
+    private router: Router
+  ) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    this.http.get(this.usersService.url + '/api/admin/validate').subscribe(
+      (res) => {
+        this.isAdmin = true;
+      },
+      (err) => {
+        this.isAdmin = false;
+        this.router.navigate(['/admin']);
+      }
+    );
+
+    if (!this.isAdmin) {
+      this.router.navigate(['/admin']);
+    }
+    console.log(this.isAdmin);
+    return this.isAdmin;
+  }
+}
