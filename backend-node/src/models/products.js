504dd05d88486af6e5e21afbce0fe456b3f07d8b
okira <omarrafat60@gmail.com>
2020-11-01 10:14:41 +0200

    Made the description field required

--- backend-node/src/models/products.js
+++ backend-node/src/models/products.js
@@ -1,28 +1,31 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-    title: {
-        required: true,
-        type: String,
-        maxlength: 30,
-        trim: true,
+const productSchema = new mongoose.Schema(
+    {
+        title: {
+            required: true,
+            type: String,
+            maxlength: 30,
+            trim: true,
+        },
+        price: {
+            required: true,
+            type: Number,
+        },
+        description: {
+            required: true,
+            type: String,
+            maxlength: 400,
+        },
+        image: {
+            required: true,
+            type: String,
+        },
     },
-    price: {
-        required: true,
-        type: Number,
-    },
-    description: {
-        required: false,
-        type: String,
-        maxlength: 400,
-    },
-    image: {
-        required: true,
-        type: String,
+    {
+        timestamps: true,
     }
-}, {
-    timestamps: true,
-});
+);
 
 const Product = mongoose.model("Product", productSchema);
 
