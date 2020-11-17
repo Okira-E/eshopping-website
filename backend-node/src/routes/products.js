e45be3a9cb48efe564ae85d3c5107c13193f7e6a
okira <omarrafat60@gmail.com>
2020-11-17 12:51:18 +0200

    Added a route to get all products

--- backend-node/src/routes/products.js
+++ backend-node/src/routes/products.js
@@ -6,28 +6,41 @@ const auth = require("../middleware/auth");
 
 const router = new express.Router();
 
-router.post(
-    "/api/products/create",
-    [auth, multer({ storage }).single("image")],
-    async (req, res) => {
-        const email = req.user.email;
-        const { title, description, price } = req.body;
+// router.post(
+//     "/api/products/create",
+//     [auth, multer({ storage }).single("image")],
+//     async (req, res) => {
+//         const email = req.user.email;
+//         const { title, description, price } = req.body;
 
-        const serverUrl = req.protocol + "://" + req.get("host");
-        const imagePath = serverUrl + "/images/" + req.file.filename;
+//         const serverUrl = req.protocol + "://" + req.get("host");
+//         const imagePath = serverUrl + "/images/" + req.file.filename;
 
-        try {
-            await User.findAdminByEmail({ email });
-            await new Product({
-                title,
-                description,
-                price,
-                image: imagePath,
-            }).save();
+//         try {
+//             await User.findAdminByEmail({ email });
+//             await new Product({
+//                 title,
+//                 description,
+//                 price,
+//                 image: imagePath,
+//             }).save();
 
-            res.status(201).send();
-        } catch {
-            res.status(403).send();
-        }
+//             res.status(201).send();
+//         } catch {
+//             res.status(403).send();
+//         }
+//     }
+// );
+
+router.get("/api/products/getProducts", auth, async (req, res) => {
+    try {
+        const products = await Product.find()
+            .skip(parseInt(req.query.skip))
+            .limit(12);
+        res.send(products);
+    } catch {
+        res.status(500).send();
     }
-);
+});
+
+module.exports = router;
