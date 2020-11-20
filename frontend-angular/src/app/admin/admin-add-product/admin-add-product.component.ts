f13fdfc3ca2e21d18ac646483571388ebd9ed776
okira <omarrafat60@gmail.com>
2020-11-20 21:57:00 +0200

    Created a new child component for creating products inside the admin

---
+++ frontend-angular/src/app/admin/admin-add-product/admin-add-product.component.ts
@@ -0,0 +1,77 @@
+import { imageValidator } from './../../services/mime-type.validator';
+import {
+  FormGroup,
+  NgForm,
+  FormControl,
+  Validators,
+  AbstractControl,
+} from '@angular/forms';
+import { UsersService } from './../../services/users.service';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-add-product',
+  templateUrl: './admin-add-product.component.html',
+  styleUrls: ['./admin-add-product.component.css'],
+})
+export class AdminAddProductComponent implements OnInit {
+  public form: FormGroup;
+  public imagePreview: string | ArrayBuffer = '';
+
+  private title: AbstractControl;
+  private description: AbstractControl;
+
+  @Output() private isCreateProductEvent = new EventEmitter<boolean>();
+  constructor(private usersService: UsersService) {}
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      image: new FormControl(null, {
+        validators: [Validators.required],
+        asyncValidators: [imageValidator],
+      }),
+      title: new FormControl(),
+      description: new FormControl(),
+      price: new FormControl(),
+    });
+  }
+
+  public toggleCreateProduct(): void {
+    setTimeout(() => {
+      this.isCreateProductEvent.emit(false);
+      this.isCreateProductEvent.unsubscribe();
+    }, 50);
+  }
+
+  public onUploadImage(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({ image: file });
+    this.form.get('image').updateValueAndValidity();
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result;
+    };
+    reader.readAsDataURL(file);
+  }
+
+  public uploadImage() {
+    const formData = new FormData();
+    if (this.form.invalid) {
+      return;
+    }
+    let newTitle = '';
+    for (const char of this.form.value.title) {
+      if (char !== ' ') {
+        newTitle += char;
+      } else {
+        newTitle += '-';
+      }
+    }
+
+    formData.append('image', this.form.value.image, `${newTitle}`);
+
+    this.usersService.uploadProductImage(formData);
+    this.toggleCreateProduct();
+  }
+}
