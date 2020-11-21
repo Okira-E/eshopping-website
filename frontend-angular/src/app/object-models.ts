c9027ef46f976c5ba40c1f388d8bab588b6a47d6
okira <omarrafat60@gmail.com>
2020-11-21 21:24:43 +0200

    Created a product model

--- frontend-angular/src/app/object-models.ts
+++ frontend-angular/src/app/object-models.ts
@@ -1,7 +1,14 @@
 export interface User {
   firstName?: string;
   lastName?: string;
-  email: string;
-  password: string;
+  email?: string;
+  password?: string;
   isAdmin?: boolean;
+}
+
+export interface Product {
+  title?: string;
+  description?: string;
+  price?: number;
+  image?: string;
 }
