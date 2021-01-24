6f35bbd9b721fb8ed157203e4d8d7f5ba63afeaa
Okira-E <omarrafat60@gmail.com>
2021-01-24 19:46:46 +0200

    Updated the max caharacters allowed for products titles

--- backend-node/src/models/products.js
+++ backend-node/src/models/products.js
@@ -5,7 +5,7 @@ const productSchema = new mongoose.Schema(
         title: {
             required: true,
             type: String,
-            maxlength: 150,
+            maxlength: 350,
             trim: true,
         },
         price: {
