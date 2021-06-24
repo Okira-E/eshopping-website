b161318b6748a738a617b839d743c379d741ebdb
okira <omarrafat60@gmail.com>
2021-06-23 21:00:02 -0400

    Connected to a mongoDB cluster

--- backend-node/src/db/mongo.js
+++ backend-node/src/db/mongo.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
 
 mongoose
-    .connect("mongodb://mongo:27017/primary", {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => {
-        console.log("Connection with MongoDB has been established!");
-    })
-    .catch(error => {
-        console.log("Connection Failed with ", error);
-    });
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connection with MongoDB has been established!");
+  })
+  .catch((error) => {
+    console.log("Connection Failed with ", error);
+  });
+
