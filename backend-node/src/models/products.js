0f00c6ad3bb84b17da96e3346202dc00fc4c943a
okira <omarrafat60@gmail.com>
2021-06-23 21:02:14 -0400

    Reformatted code

--- backend-node/src/models/products.js
+++ backend-node/src/models/products.js
@@ -1,34 +1,33 @@
 const mongoose = require("mongoose");
-
 const productSchema = new mongoose.Schema(
-    {
-        title: {
-            required: true,
-            type: String,
-            maxlength: 350,
-            trim: true,
-        },
-        price: {
-            required: true,
-            type: Number,
-        },
-        description: {
-            required: true,
-            type: String,
-            maxlength: 1400,
-        },
-        image: {
-            required: true,
-            type: String,
-        },
-        category: {
-            required: true,
-            type: String,
-        },
+  {
+    title: {
+      required: true,
+      type: String,
+      maxlength: 350,
+      trim: true,
     },
-    {
-        timestamps: true,
-    }
+    price: {
+      required: true,
+      type: Number,
+    },
+    description: {
+      required: true,
+      type: String,
+      maxlength: 1400,
+    },
+    image: {
+      required: true,
+      type: String,
+    },
+    category: {
+      required: true,
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 const Product = mongoose.model("Product", productSchema);
