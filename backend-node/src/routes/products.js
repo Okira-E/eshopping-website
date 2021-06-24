c628c2d45457be886f13ca2af4b44a475578d965
okira <omarrafat60@gmail.com>
2021-06-23 21:02:04 -0400

    Reformatted code

--- backend-node/src/routes/products.js
+++ backend-node/src/routes/products.js
@@ -13,82 +13,82 @@ const router = new express.Router();
 const deleteImageAsync = promisify(fs.unlink);
 
 router.post(
-    "/api/products/create",
-    [auth, multer({ storage }).single("image")],
-    async (req, res) => {
-        const { email } = req.user;
-        const { title, description, price, category } = req.body;
+  "/api/products/create",
+  [auth, multer({ storage }).single("image")],
+  async (req, res) => {
+    const { email } = req.user;
+    const { title, description, price, category } = req.body;
 
-        const serverUrl = req.protocol + "://" + req.get("host");
-        const imagePath = serverUrl + "/images/" + req.file.filename;
+    const serverUrl = req.protocol + "://" + req.get("host");
+    const imagePath = serverUrl + "/images/" + req.file.filename;
 
-        try {
-            await User.findAdminByEmail(email);
-            await new Product({
-                title,
-                description,
-                price,
-                image: imagePath,
-                category,
-            }).save();
+    try {
+      await User.findAdminByEmail(email);
+      await new Product({
+        title,
+        description,
+        price,
+        image: imagePath,
+        category,
+      }).save();
 
-            res.status(201).send();
-        } catch (err) {
-            console.log(err);
-            res.status(403).send();
-        }
+      res.status(201).send();
+    } catch (err) {
+      console.log(err);
+      res.status(403).send();
     }
+  }
 );
 
 router.post("/api/products/delete", auth, async (req, res) => {
-    try {
-        await deleteImageAsync(
-            req.body.image.replace("http://0.0.0.0:3200/", "./")
-        );
-        await Product.deleteOne({
-            title: req.body.title,
-            description: req.body.description,
-            price: req.body.price,
-        });
-        res.status(200).send();
-    } catch (err) {
-        console.log(err);
-        res.status(500).send();
-    }
+  try {
+    await deleteImageAsync(
+      req.body.image.replace("http://0.0.0.0:3200/", "./")
+    );
+    await Product.deleteOne({
+      title: req.body.title,
+      description: req.body.description,
+      price: req.body.price,
+    });
+    res.status(200).send();
+  } catch (err) {
+    console.log(err);
+    res.status(500).send();
+  }
 });
 
 router.get("/api/products/getProducts", auth, async (req, res) => {
-    try {
-        if (req.query.skip) {
-            var products = await Product.find()
-                .skip(parseInt(req.query.skip))
-                .limit(12);
-        } else {
-            var products = await Product.find();
-        }
-        res.send(products);
-    } catch {
-        res.status(500).send();
+  try {
+    if (req.query.skip) {
+      var products = await Product.find()
+        .skip(parseInt(req.query.skip))
+        .limit(12);
+    } else {
+      var products = await Product.find();
     }
+    res.send(products);
+  } catch {
+    res.status(500).send();
+  }
 });
 
 router.get("/api/products/getProductsByCategory", auth, async (req, res) => {
-    try {
-        // for (let category of categories) {
-        //     let products = [];
-        //     if ((req.query.category = category)) {
-        //         let resultProduct = await Product.find({ category });
-        //         products.push(resultProduct);
-        //     }
-        //     break;
-        // }
-        console.log(req.query.category);
-        const products = await Product.find({ category: req.query.category });
-        console.log(products);
-        res.send(products);
-    } catch {
-        res.status(500).send();
-    }
+  try {
+    // for (let category of categories) {
+    //     let products = [];
+    //     if ((req.query.category = category)) {
+    //         let resultProduct = await Product.find({ category });
+    //         products.push(resultProduct);
+    //     }
+    //     break;
+    // }
+    console.log(req.query.category);
+    const products = await Product.find({ category: req.query.category });
+    console.log(products);
+    res.send(products);
+  } catch {
+    res.status(500).send();
+  }
 });
 
 module.exports = router;
